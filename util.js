3c4220df8aec1dea410540005b3fd8f1bdd8baba
Edgar Patzan <patzan.007@gmail.com>
2016-08-04 18:41:39 -0600

    firts commit

---
+++ util.js
@@ -0,0 +1,12 @@
+'use strict';
+module.exports = {
+	PROJECT_DIR: __dirname,
+	CONFIG: {
+		ENV: 'dev',
+		COMPONENTS: __dirname + '/shell/modules/',
+		BASE_MAIN: 'http://192.168.1.12:5030/',
+		PORT: 5030,
+		THIRD_PARTY: {
+		}
+	}
+}
